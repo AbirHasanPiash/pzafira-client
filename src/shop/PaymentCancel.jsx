6d03d53500d04878c88e162ebf882bb37d73e081
Abir Hasan Piash <abirhasanpiash@gmail.com>
2025-05-26 17:50:13 +0600

    need to create another branch to update feature

--- src/shop/PaymentCancel.jsx
+++ src/shop/PaymentCancel.jsx
@@ -2,7 +2,7 @@ import { FaExclamationTriangle } from "react-icons/fa";
 
 const PaymentCancel = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-yellow-50 px-4">
+    <div className="flex items-center justify-center min-h-screen px-4">
       <div className="text-center p-6 md:p-10 rounded-2xl shadow-xl bg-white w-full max-w-md">
         <FaExclamationTriangle className="text-yellow-500 text-6xl md:text-7xl mx-auto mb-4" />
         <h1 className="text-xl md:text-2xl font-bold mb-2">Payment Cancelled</h1>
